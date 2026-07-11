f17daadfdb5e83e84192107aeddda47e7d810ff8
Rom <romain.silande@gmail.com>
2026-07-11 20:45:30 +0200

    Initial Commit

---
+++ FUDO/FUDOApp.swift
@@ -0,0 +1,17 @@
+//
+//  FUDOApp.swift
+//  FUDO
+//
+//  Created by Romain on 11/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct FUDOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FUDOTests/FUDOTests.swift
@@ -0,0 +1,19 @@
+//
+//  FUDOTests.swift
+//  FUDOTests
+//
+//  Created by Romain on 11/07/2026.
+//
+
+import Testing
+@testable import FUDO
+
+struct FUDOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
